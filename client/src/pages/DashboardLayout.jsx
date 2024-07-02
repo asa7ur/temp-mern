e3d3e353eacd999c53270e74b603d45db7f24b4e
Garik Asatryan <asa7ur@gmail.com>
2024-07-02 14:46:57 +0200

    first commit

---
+++ client/src/pages/DashboardLayout.jsx
@@ -0,0 +1,92 @@
+import styled from 'styled-components'
+import { createContext, useState, useContext } from 'react'
+import { Outlet, redirect, useLoaderData, useNavigate } from 'react-router-dom'
+import { BigSidebar, SmallSidebar, Navbar } from '../components'
+import customFetch from '../utils/customFetch'
+import { toast } from 'react-toastify'
+
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get('/users/current-user')
+    return data
+  } catch (error) {
+    return redirect('/')
+  }
+}
+
+const DashboardContext = createContext()
+
+const DashboardLayout = ({ isDarkThemeEnabled }) => {
+  const { user } = useLoaderData()
+  const navigate = useNavigate()
+  const [showSidebar, setShowSidebar] = useState(false)
+  const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled)
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme
+    setIsDarkTheme(newDarkTheme)
+    document.body.classList.toggle('dark-theme', newDarkTheme)
+    localStorage.setItem('darkTheme', newDarkTheme)
+  }
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar)
+  }
+
+  const logoutUser = async () => {
+    navigate('/')
+    await customFetch.get('/auth/logout')
+    toast.success('Logging out...')
+  }
+
+  return (
+    <DashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+        toggleSidebar,
+        logoutUser,
+      }}
+    >
+      <Wrapper>
+        <main className='dashboard'>
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className='dashboard-page'>
+              <Outlet context={{ user }} />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
+  )
+}
+export const useDashboardContext = () => useContext(DashboardContext)
+export default DashboardLayout
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+
+  @media (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,104 @@
+import { Link, Form, redirect, useNavigate, useActionData } from 'react-router-dom'
+import styled from 'styled-components'
+import { FormRow, Logo, SubmitBtn } from '../components'
+import customFetch from '../utils/customFetch'
+import { toast } from 'react-toastify'
+
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  // const errors = { msg: '' }
+  // if (data.password.length < 6) {
+  //   errors.msg = 'password too short'
+  //   return errors
+  // }
+  try {
+    await customFetch.post('/auth/login', data)
+    toast.success('Login successful')
+    return redirect('/dashboard')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    // errors.msg = error?.response?.data?.msg
+    return error
+  }
+}
+
+const Login = () => {
+  // const errors = useActionData()
+
+  const navigate = useNavigate()
+  const loginDemoUser = async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'secret123',
+    }
+    try {
+      await customFetch.post('/auth/login', data)
+      toast.success('Take a test drive!')
+      navigate('/dashboard')
+    } catch (error) {
+      toast.error(error?.response?.data?.msg)
+    }
+  }
+
+  return (
+    <Wrapper>
+      <Form method='post' className='form'>
+        <Logo />
+        <h4>login</h4>
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
+        {/* {errors?.msg && <p style={{color: 'red'}}>{errors?.msg}</p>} */}
+        <SubmitBtn />
+        <button type='button' className='btn btn-block' onClick={loginDemoUser}>
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
+  )
+}
+export default Login
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h4 {
+    text-align: center;
+    margin-bottom: 1.38rem;
+  }
+
+  p {
+    margin-top: 1rem;
+    text-align: center;
+    line-height: 1.5;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letter-spacing);
+    margin-left: 0.25rem;
+  }
+`
