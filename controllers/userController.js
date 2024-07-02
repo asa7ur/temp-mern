1014fa02dd9dbdd88d9ca76bbef6a265c3e598a2
Garik Asatryan <asa7ur@gmail.com>
2024-07-02 15:50:28 +0200

    bug fixes

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,16 +9,15 @@ export const getCurrentUser = async (req, res) => {
   const userWithoutPassword = user.toJSON()
   res.status(StatusCodes.OK).json({ user: userWithoutPassword })
 }
-
 export const getApplicationStats = async (req, res) => {
   const users = await User.countDocuments()
   const jobs = await Job.countDocuments()
   res.status(StatusCodes.OK).json({ users, jobs })
 }
-
 export const updateUser = async (req, res) => {
   const newUser = { ...req.body }
   delete newUser.password
+  delete newUser.role
 
   if (req.file) {
     const file = formatImage(req.file)
@@ -29,7 +28,7 @@ export const updateUser = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser)
 
   if (req.file && updatedUser.avatarPublicId) {
-    await cloudinary.vs.uploader.destroy(updateUser.avatarPublicId)
+    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId)
   }
 
   res.status(StatusCodes.OK).json({ msg: 'update user' })
